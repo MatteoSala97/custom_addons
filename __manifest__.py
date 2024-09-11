8ab898e5c2c4e7e683329f9f8a74d1b89ebc07b9
MatteoSala97 <matteo111997@gmail.com>
2024-09-11 14:40:59 +0000

    created test table and updated manifest

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'User Table',
+    'version': '1.0',
+    'summary': 'A module to create a user table',
+    'description': 'This module adds a simple user table with name, email, and username fields.',
+    'author': 'Matteo',
+    'depends': ['base'],
+    'data': [
+        'views/user_table_view.xml'        
+        ],
+    'installable': True,
+    'application': True,
+}
--- models/user_table.py
+++ models/user_table.py
@@ -4,4 +4,6 @@ class UserTable(models.Model):
     _name = 'user.table'
     _description = 'Modulo di test con tabella'
     
-    name = fields.Char(string='Nome')
+    name = fields.Char(string='Name', required=True)
+    email = fields.Char(string='Email', required=True)
+    username = fields.Char(string='Username', required=True)
